ddac1a6a58479c325424e67f194aefb98d6ced1c
Saabir Hussein <saabirhus2003@gmail.com>
2025-07-20 00:41:26 +0100

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import mysql from "mysql2";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "root1234",
-  database: "test",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 app.use(express.json());
